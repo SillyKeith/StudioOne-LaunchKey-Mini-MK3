e5f2d42547840ec19c788652f02b03ac0c0a3238
Keith Kepler <28606059+SillyKeith@users.noreply.github.com>
2024-06-21 09:48:26 -0700

    Refactored code to be a Class and be more like the rest of Presonus's extensions.  No new functionality is added

--- LaunchKeyMKIIIComponent_basic.js
+++ LaunchKeyMKIIIComponent_basic.js
@@ -3,53 +3,50 @@
  * @Date:   2020-05-27T21:56:33+09:30
  * @Email:  CQoute@gmail.com
  * @Filename: LaunchKeyMKIIIComponent.js
- * @Last modified by:   Sian Croser <Sian-Lee-SA>
- * @Last modified time: 2020-05-28T10:02:56+09:30
+ * @Last modified by:   Keith Kepler <SillyKeith>
+ * @Last modified time: 2024-05-27T21:56:33+09:30
  * @License: GPL-3
  */
 // include SDK files from host
+// include_file("resource://com.presonus.musicdevices/sdk/controlsurfacecomponent.js");
+// include_file("resource://com.presonus.musicdevices/sdk/controlsurfacedevice.js");
 include_file("./resources/controlsurfacecomponent.js");
 include_file("Debug.js");
 include_file("Modes.js");
 
-LaunchKeyMK3BasicComponent.prototype = new PreSonus.ControlSurfaceComponent ();
-function LaunchKeyMK3BasicComponent ()
-{
+class LaunchKeyMK3BasicComponent extends PreSonus.ControlSurfaceComponent {
+    constructor() {
+        super();
+        this.interfaces = [Host.Interfaces.IObserver, Host.Interfaces.IParamObserver];
+    }
 
-    this.interfaces = [Host.Interfaces.IObserver,
-                       Host.Interfaces.IParamObserver];
+    onInit(hostComponent) {
+        super.onInit(hostComponent);
 
-    this.onInit = function (hostComponent)
-    {
-        PreSonus.ControlSurfaceComponent.prototype.onInit.call (this, hostComponent);
+        this.model = hostComponent.model;
 
-        this.model = 	            hostComponent.model;
+        let paramList = hostComponent.paramList;
+        this.shiftModifier = paramList.addParam("shiftModifier");
+        this.sceneHold = paramList.addParam("sceneHold");
 
-        let paramList = 		    hostComponent.paramList;
-        this.shiftModifier = 	    paramList.addParam("shiftModifier");
-        this.sceneHold = 	        paramList.addParam("sceneHold");
-
-        this.modes = new Modes( hostComponent );
+        this.modes = new Modes(hostComponent);
         Host.Signals.advise("LaunchkeyMK3", this);
     }
 
-    this.paramChanged = function (param)
-    {
+    paramChanged(param) {
+        // Implementation needed if there's specific logic for paramChanged
     }
 
-    this.onSelectPressed = function(state)
-    {
+    onSelectPressed(state) {
         let commandName = this.shiftModifier.value ? "Cancel" : "Enter";
-        Host.GUI.Commands.deferCommand ("Navigation", commandName);
+        Host.GUI.Commands.deferCommand("Navigation", commandName);
     }
 
-    this.notify = function(subject, msg)
-    {
-        if( msg.id == 'paramChanged')
-        {
-            if( this[msg.getArg(0).name] )
+    notify(subject, msg) {
+        if (msg.id == 'paramChanged') {
+            if (this[msg.getArg(0).name])
                 this[msg.getArg(0).name].setValue(msg.getArg(0).value, true);
-            else if( this.modes.params[msg.getArg(0).name] )
+            else if (this.modes.params[msg.getArg(0).name])
                 this.modes.params[msg.getArg(0).name].setValue(msg.getArg(0).value, true);
         }
     }

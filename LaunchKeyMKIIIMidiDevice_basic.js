669aff013307eeb4b9ce99706b2688159e995a8c
Keith Kepler <28606059+SillyKeith@users.noreply.github.com>
2024-06-22 11:57:08 -0700

    finish converting functions into classes

--- LaunchKeyMKIIIMidiDevice_basic.js
+++ LaunchKeyMKIIIMidiDevice_basic.js
@@ -13,26 +13,26 @@ include_file("./resources/midiprotocol.js");
 include_file("./resources/controlsurfacedevice.js");
 include_file("Debug.js");
 
-TouchModHandler.prototype = new PreSonus.ControlHandler();
-function TouchModHandler(name, channel)
-{
-    this.name = name;
-    this.status = 0xB0;
-    this.channel = channel - 1;
-    this.address = 1;
+class TouchModHandler extends PreSonus.ControlHandler {
+    constructor(name, channel) {
+        super();
+        this.name = name;
+        this.status = 0xB0;
+        this.channel = channel - 1;
+        this.address = 1;
 
-    this.lastValue = 0;
-    this.counter = 0;
+        this.lastValue = 0;
+        this.counter = 0;
+    }
 
-    this.receiveMidi = function(status, address, value)
-    {
-        if( status != (this.status|this.channel) || address != this.address )
+    receiveMidi(status, address, value) {
+        if (status != (this.status | this.channel) || address != this.address)
             return false;
 
-        this.counter += ( value > this.lastValue ) ? 1 : -1;
+        this.counter += (value > this.lastValue) ? 1 : -1;
         this.lastValue = value;
 
-        if( Math.abs(this.counter) < 10 )
+        if (Math.abs(this.counter) < 10)
             return true;
 
         // Divide by 10 as counter could be positive or negative
@@ -42,98 +42,91 @@ function TouchModHandler(name, channel)
         this.counter = 0;
         return true;
     }
+}
 
-};
+class TouchPitchHandler extends PreSonus.ControlHandler {
+    constructor(name, channel) {
+        super();
+        this.name = name;
+        this.status = 0xE0;
+        this.channel = channel - 1;
+        this.lastValue = [0, 0, 0];
+        this.counter = 0;
+    }
 
-TouchPitchHandler.prototype = new PreSonus.ControlHandler();
-function TouchPitchHandler(name, channel)
-{
-    this.name = name;
-    this.status = 0xE0;
-    this.channel = channel - 1;
-
-    this.lastValue = [0, 0, 0];
-    this.counter = 0;
-
-    this.receiveMidi = function(status, address, value)
-    {
-        if( status != (this.status|this.channel) )
+    receiveMidi(status, address, value) {
+        if (status != (this.status | this.channel))
             return false;
 
-        let combined_value = (value << 7)|address;
+        let combined_value = (value << 7) | address;
 
-        // We assume the pitch whell has been released as the resolution
+        // We assume the pitch wheel has been released as the resolution
         // stays the same when returning
-        if( address == this.lastValue[0] )
-        {
+        if (address == this.lastValue[0]) {
             this.counter = 0;
             return true;
         }
 
-        let dir = ( combined_value > this.lastValue[1] ) ? 1 : -1;
+        let dir = (combined_value > this.lastValue[1]) ? 1 : -1;
 
-        if( this.lastValue[2] != dir )
+        if (this.lastValue[2] != dir)
             this.counter = 0;
         else
             this.counter += dir;
 
         this.lastValue = [address, combined_value, dir];
 
-        if( Math.abs(this.counter) < 8 )
+        if (Math.abs(this.counter) < 8)
             return true;
 
-        // Divide by 10 as counter could be positive or negative
+        // Divide by 8 as counter could be positive or negative
         // Giving a result of 1 or -1
         this.updateValue(this.counter / 8);
 
         this.counter = 0;
         return true;
     }
-};
+}
 
-TouchDoubleTapHandler.prototype = new PreSonus.ControlHandler();
-function TouchDoubleTapHandler(name, channel)
-{
-    this.name = name;
-    this.status = 0xE0;
-    this.channel = channel - 1;
+class TouchDoubleTapHandler extends PreSonus.ControlHandler {
+    constructor(name, channel) {
+        super();
+        this.name = name;
+        this.status = 0xE0;
+        this.channel = channel - 1;
 
-    this.lastAddressValue = 0x00;
-    this.touchStartTime = 0;
-    this.touchEndTime = 0;
+        this.lastAddressValue = 0x00;
+        this.touchStartTime = 0;
+        this.touchEndTime = 0;
 
-    this.tapCount = 0;
-    this.time = 0;
+        this.tapCount = 0;
+        this.time = 0;
+    }
 
-    this.onIdle = function(time)
-    {
+    onIdle(time) {
         this.time = time;
     }
 
-    this.receiveMidi = function(status, address, value)
-    {
-        if( status != (this.status|this.channel) )
+    receiveMidi(status, address, value) {
+        if (status != (this.status | this.channel))
             return false;
 
-        if( this.lastAddressValue == 0x00 )
-        {
-            if( this.time - this.touchEndTime > 250 )
+        if (this.lastAddressValue == 0x00) {
+            if (this.time - this.touchEndTime > 250)
                 this.tapCount = 0;
             this.touchStartTime = this.time;
         }
 
-        if( address == 0x00 )
-        {
+        if (address == 0x00) {
             this.touchEndTime = this.time;
-            if( this.touchEndTime - this.touchStartTime < 250 )
+            if (this.touchEndTime - this.touchStartTime < 250)
                 this.tapCount++;
             else
                 this.tapCount = 0;
         }
 
         this.lastAddressValue = address;
-        if( this.tapCount >= 2 )
-        {
+        if (this.tapCount >= 2) {
             this.updateValue(1);
             this.tapCount = 0;
             return true;
